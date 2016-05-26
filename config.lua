f1962fb65e3d7669f81591fdb2aafffcf82583ea
jarriztg <jarriztg@users.noreply.github.com>
2016-05-26 06:19:45 -0500

    old

--- config.lua
+++ config.lua
@@ -1,16 +1,18 @@
 return {
-	bot_api_key = 'api',
+	bot_api_key = '',
 	admin = 0,
-	support = 0, -- id de soporte (opcional)
-	channel = '@MoDeRaToRz', -- tu canal (opcional)
-	lang = 'languages.lua',
 	log_chat = -122946087,
+	channel = '@MoDeRaToRz',
+	lang = 'languages.lua',
 	plugins = {
 		'onmessage.lua',
+		'sera.lua',
 		'all.lua',
+		'mod.lua',
 		'banhammer.lua',
 		'users.lua',
 		'help.lua',
+    	'error.lua',
 		'rules.lua',
 		'settings.lua',
 		'about.lua',
@@ -22,11 +24,9 @@ return {
 		'setlang.lua',
 		'floodmanager.lua',
 		'mediasettings.lua',
-		'private.lua',
 		'admin.lua',
-		'test.lua',
-		'faq.lua',
 		'caracola.lua',
+		'say.lua',
 		'comprimirlink.lua',
 		'sendfiles.lua',
 		'killer.lua',
@@ -35,14 +35,14 @@ return {
 		'italic.lua',
 		'contact.lua',
 	--	'spam.lua',
-		
+		'kickme.lua',
 	},
 	available_languages = {
 		'es'
-		
-		},
-		
+	},
 	chat_data = {
+		'mod',
+		'owner',
 		'settings',
 		'about',
 		'rules',
@@ -61,7 +61,6 @@ return {
 		[105] = 'Bad Request: Need to be inviter of the user to kick it from the group', 
 		[106] = 'USER_NOT_PARTICIPANT', 
 		[107] = 'CHAT_ADMIN_REQUIRED', 
-		[108] = 'there is no administrators in the private chat', 
 		
 		[110] = 'PEER_ID_INVALID', 
 		[111] = 'Bad Request: message is not modified', 
@@ -74,8 +73,8 @@ return {
 		[118] = 'Message is too long', 
 		[119] = 'User not found', 
 		
-		[120] = 'Can\'t parse reply keyboard markup JSON object',
-		[121] = 'Field \\\"inline_keyboard\\\" of the InlineKeyboardMarkup should be an Array of Arrays', 
+		[120] = 'Can\'t parse reply keyboard markup JSON object', 
+		[121] = 'Field \"inline_keyboard\" of the InlineKeyboardMarkup should be an Array of Arrays', 
 		[122] = 'Can\'t parse inline keyboard button: InlineKeyboardButton should be an Object',
 		[123] = 'Bad Request: Object expected as reply markup', 
 		[124] = 'QUERY_ID_INVALID', 
@@ -84,6 +83,5 @@ return {
 		
 		[403] = 'Bot was blocked by the user', 
 		[429] = 'Too many requests: retry later', 
-		[430] = 'Too big total timeout', 
 	}
 }
